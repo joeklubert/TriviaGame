c3d512304afdb0f552a11c1076d0f90698b46ec6
joe klubert <joeklubert@gmail.com>
2017-11-17 20:20:49 -0500

    idex.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+
+function myFunction() {
+    document.getElementById("question").innerHTML ;
+}
+
+$(document).ready(function(){
+    $(".game").onload(function(){
+        $("#nextQuestion").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#gameStart").click(function(){
+        $("question").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+function  myquestion(question){
+
+
+var i = [
+	{
+		question: "What is division by zero?",
+		answers: {
+			a: '0',
+			b: '1/x',
+			c: 'undefined'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Who is buried in Grant's tomb",
+		answers: {
+			a: 'Grant',
+			b: 'no one',
+			c: 'Lou Grant'
+		},
+		correctAnswer: 'a'
+	}
+];
+	
